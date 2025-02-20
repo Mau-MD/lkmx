990adb6ced3d4eeb03c620b312ceae55ca5ff4f4
Mauricio Munoz <j.mauricio.munoz1@gmail.com>
2025-02-19 17:09:19 -0800

    Better error handling when server returns error

--- src/client/handlers/api.ts
+++ src/client/handlers/api.ts
@@ -1,15 +1,81 @@
 export type Result<T> = [T | null, string | null];
-// This function is used to handle errors in the API. If the API call is successful, the function will return the data and null. If the API call is not successful, the function will return null and the error message.
-export async function withErrorHandling<T>(
-  fn: () => Promise<T>,
-): Promise<Result<T>> {
+
+export async function GET<T>(url: string): Promise<Result<T>> {
   try {
-    const result = await fn();
-    return [result, null];
+    const res = await fetch(url);
+    if (!res.ok) {
+      console.error(res.statusText);
+      return [null, res.statusText];
+    }
+    return [await res.json(), null];
   } catch (error) {
     console.error(error);
     if (error instanceof Error) {
-      console.error(error.message);
+      return [null, error.message];
+    }
+    return [null, "An unknown error occurred"];
+  }
+}
+
+export async function POST<T>(
+  url: string,
+  body: Record<string, unknown>
+): Promise<Result<T>> {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      console.error(res.statusText);
+      return [null, res.statusText];
+    }
+    return [await res.json(), null];
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return [null, error.message];
+    }
+    return [null, "An unknown error occurred"];
+  }
+}
+
+export async function DELETE<T>(url: string): Promise<Result<T>> {
+  try {
+    const res = await fetch(url, {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      console.error(res.statusText);
+      return [null, res.statusText];
+    }
+    return [await res.json(), null];
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return [null, error.message];
+    }
+    return [null, "An unknown error occurred"];
+  }
+}
+
+export async function PUT<T>(
+  url: string,
+  body: Record<string, unknown>
+): Promise<Result<T>> {
+  try {
+    const res = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      console.error(res.statusText);
+      return [null, res.statusText];
+    }
+    return [await res.json(), null];
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
       return [null, error.message];
     }
     return [null, "An unknown error occurred"];
--- src/client/handlers/userapi.ts
+++ src/client/handlers/userapi.ts
@@ -1,45 +1,25 @@
 import { SERVER_URL } from "@/shared/config";
 import { UserAnalytics, UserInput, UserOutput } from "@/shared/types";
-import { Result, withErrorHandling } from "./api";
+import { DELETE, GET, POST, Result } from "./api";
 
 export async function getUsers(): Promise<Result<UserOutput[]>> {
-  return await withErrorHandling(async () => {
-    const res = await fetch(`${SERVER_URL}/api/users`);
-    return (await res.json()) as UserOutput[];
-  });
+  return await GET<UserOutput[]>(`${SERVER_URL}/api/users`);
 }
 
 export async function getUserAnalytics(): Promise<Result<UserAnalytics>> {
-  return await withErrorHandling(async () => {
-    const res = await fetch(`${SERVER_URL}/api/users/analytics`);
-    return (await res.json()) as UserAnalytics;
-  });
+  return await GET<UserAnalytics>(`${SERVER_URL}/api/users/analytics`);
 }
 
 export async function createUser(user: UserInput): Promise<Result<void>> {
-  return await withErrorHandling(async () => {
-    await fetch(`${SERVER_URL}/api/users`, {
-      method: "POST",
-      body: JSON.stringify(user),
-    });
-  });
+  return await POST<void>(`${SERVER_URL}/api/users`, user);
 }
 
 export async function updateUser(
   userId: number,
   user: UserInput
 ): Promise<Result<void>> {
-  return await withErrorHandling(async () => {
-    await fetch(`${SERVER_URL}/api/users/${userId}`, {
-      method: "PUT",
-      body: JSON.stringify(user),
-    });
-  });
+  return await POST<void>(`${SERVER_URL}/api/users/${userId}`, user);
 }
 export async function deleteUser(id: string): Promise<Result<void>> {
-  return await withErrorHandling(async () => {
-    await fetch(`${SERVER_URL}/api/users/${id}`, {
-      method: "DELETE",
-    });
-  });
+  return await DELETE<void>(`${SERVER_URL}/api/users/${id}`);
 }
