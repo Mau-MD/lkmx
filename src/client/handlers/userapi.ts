9e83894f17be133f1fd5720bab67dd50e9b8b4b0
Mauricio Munoz <j.mauricio.munoz1@gmail.com>
2025-02-19 17:11:32 -0800

    Fix Update called POST instead of PUT

--- src/client/handlers/userapi.ts
+++ src/client/handlers/userapi.ts
@@ -1,6 +1,6 @@
 import { SERVER_URL } from "@/shared/config";
 import { UserAnalytics, UserInput, UserOutput } from "@/shared/types";
-import { DELETE, GET, POST, Result } from "./api";
+import { DELETE, GET, POST, PUT, Result } from "./api";
 
 export async function getUsers(): Promise<Result<UserOutput[]>> {
   return await GET<UserOutput[]>(`${SERVER_URL}/api/users`);
@@ -18,7 +18,7 @@ export async function updateUser(
   userId: number,
   user: UserInput
 ): Promise<Result<void>> {
-  return await POST<void>(`${SERVER_URL}/api/users/${userId}`, user);
+  return await PUT<void>(`${SERVER_URL}/api/users/${userId}`, user);
 }
 export async function deleteUser(id: string): Promise<Result<void>> {
   return await DELETE<void>(`${SERVER_URL}/api/users/${id}`);
